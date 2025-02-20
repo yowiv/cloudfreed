ebe52e6acdb6f676b57aff1fd252642ce3697409
zetxtech <i@zetx.tech>
2025-02-21 00:55:14 +0800

    Update

--- README.md
+++ README.md
@@ -4,24 +4,29 @@ Updates are on the way!
 V4 Just released, please report any errors in Issues.
 
 ## Discord Server
+
 [![Discord](https://img.shields.io/discord/1219437599740002445.svg?label=Discord&logo=discord&colorB=7289DA)](https://discord.gg/5ncqYYQTPN)
 
 ## Notice
+
 If you like the repo, please consider starring it, starring repos will help it spread.
 CloudFreed is 100% Free, CloudFreed can stop working at any time.
 Files may have problems loading correctly, this is because of the request interception. This may be fixed in the future!
 
 ## Introduction
+
 <div style="text-align:center;">
   <img src="html/CloudFreed.png" alt="CloudFreed Logo" width="48" style="float:left; margin-right:10px;">
   <h1>CloudFreed V4</h1>
 
-  [Join the CloudFreed Server](https://discord.gg/8F852cXVbX)
+[Join the CloudFreed Server](https://discord.gg/8F852cXVbX)
+
 </div>
 
 CloudFreed is a powerful tool designed to bypass Cloudflare anti-bot protection, allowing users to access websites without being restricted by captchas or Cloudflare's security measures.
 
 ## Installation
+
 Before using CloudFreed, ensure that you have Node.js installed on your system. If not, you can download and install it from [Node.js website](https://nodejs.org/).
 
 Once Node.js is installed, follow these steps to set up CloudFreed:
@@ -31,19 +36,24 @@ Once Node.js is installed, follow these steps to set up CloudFreed:
 3. Open a terminal and navigate to the directory where you have cloned/downloaded CloudFreed.
 4. Run the following command to install dependencies:
 
-    ```
-    npm i
-    ```
-    alternatively, you can use:
-    ```
-    npm install
-    ```
+   ```
+   npm i
+   ```
+
+   alternatively, you can use:
+
+   ```
+   npm install
+   ```
+
 ## Usage
+
 After installing dependencies, you can start using CloudFreed. The main functionality is provided through the `CloudFreed` function, which returns the `cf_clearence` cookie necessary for bypassing Cloudflare protection.
 
 To use CloudFreed in your project, look at example.js
 
 ### Example:
+
 Find example in example.js
 
 ## Contribution
@@ -60,4 +70,5 @@ Please consider donating if you like the project.
 ---
 
 ## Note
+
 CloudFreed is intended for educational and research purposes only. Please use it responsibly and respect the terms of service of the websites you visit.
--- example.js
+++ example.js
@@ -1,5 +1,5 @@
-import CloudFreed from "./index.js"
-import delay from "./lib/delay.js"
+import CloudFreed from "./lib/CloudFreed.js";
+import delay from "./lib/delay.js";
 
 /* 
 documentation for instance.Solve data.
@@ -20,69 +20,87 @@ documentation for instance.Solve data.
 }) => Promise<...>) | undefined
 */
 
-const CF = new CloudFreed()
+const CF = new CloudFreed();
 
-const instance = await CF.start(false, true)
+const instance = await CF.start(false, true);
 
-console.log(instance)
+console.log(instance);
 
-console.log(await instance.Solve({
-  type: "V3",
-  url: "www.subber.xyz",
-  proxy: { scheme: "http", host: "127.0.0.1", port: 8080 },
-}))
+console.log(
+  await instance.Solve({
+    type: "V3",
+    url: "https://vip.charontv.com/challenge?id=123456",
+    proxy: { scheme: "http", host: "127.0.0.1", port: 8080 },
+  }),
+);
 
-await delay(3723498) // optional, stop (kinda) after testing V3.
+await delay(3723498); // optional, stop (kinda) after testing V3.
 
-console.log(await instance.Solve({
-  type: "Turnstile",
-  url: "https://web.nebula-media.org",
-  sitekey: "0x4AAAAAAADGwT_-TpuCDrw9",
-  proxy: { scheme: "socks5", host: "127.0.0.1", port: 1080 },
-}))
+console.log(
+  await instance.Solve({
+    type: "Turnstile",
+    url: "https://web.nebula-media.org",
+    sitekey: "0x4AAAAAAADGwT_-TpuCDrw9",
+    proxy: { scheme: "socks5", host: "127.0.0.1", port: 1080 },
+  }),
+);
 
-console.log(await instance.Solve({
-  type: "RecaptchaInvisible",
-  url: "https://antcpt.com/score_detector/",
-  sitekey: "6LcR_okUAAAAAPYrPe-HK_0RULO1aZM15ENyM-Mf",
-  action: "homepage",
-  //proxy: { scheme: "http", host: "152.26.229.42", port: 9443 },
-  userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
-}))
+console.log(
+  await instance.Solve({
+    type: "RecaptchaInvisible",
+    url: "https://antcpt.com/score_detector/",
+    sitekey: "6LcR_okUAAAAAPYrPe-HK_0RULO1aZM15ENyM-Mf",
+    action: "homepage",
+    //proxy: { scheme: "http", host: "152.26.229.42", port: 9443 },
+    userAgent:
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+  }),
+);
 
-console.log(await instance.Solve({
-  type: "Invisible",
-  url: "discord.com",
-  //proxy: { scheme: "http", host: "152.26.229.42", port: 9443 },
-  userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
-}))
+console.log(
+  await instance.Solve({
+    type: "Invisible",
+    url: "discord.com",
+    //proxy: { scheme: "http", host: "152.26.229.42", port: 9443 },
+    userAgent:
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+  }),
+);
 
-console.log(await instance.Solve({
-  type: "IUAM",
-  url: "bloxmoon.com",
-  //proxy: { scheme: "http", host: "152.26.229.42", port: 9443 },
-  userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
-}))
+console.log(
+  await instance.Solve({
+    type: "IUAM",
+    url: "bloxmoon.com",
+    //proxy: { scheme: "http", host: "152.26.229.42", port: 9443 },
+    userAgent:
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+  }),
+);
 
-console.log(await instance.Solve({
-  type: "V3",
-  url: "www.subber.xyz",
-  proxy: { scheme: "socks5", host: "127.0.0.1", port: 1080 },
-}))
+console.log(
+  await instance.Solve({
+    type: "V3",
+    url: "www.subber.xyz",
+    proxy: { scheme: "socks5", host: "127.0.0.1", port: 1080 },
+  }),
+);
 
-console.log(await instance.Solve({
-  type: "Turnstile",
-  url: "www.coronausa.com",
-  sitekey: "0x4AAAAAAAH4-VmiV_O_wBN-",
-  proxy: { scheme: "socks5", host: "127.0.0.1", port: 1080 },
-}))
+console.log(
+  await instance.Solve({
+    type: "Turnstile",
+    url: "www.coronausa.com",
+    sitekey: "0x4AAAAAAAH4-VmiV_O_wBN-",
+    proxy: { scheme: "socks5", host: "127.0.0.1", port: 1080 },
+  }),
+);
 
-console.log(await instance.Solve({
-  type: "Turnstile",
-  url: "https://web.nebula-media.org",
-  sitekey: "0x4AAAAAAADGwT_-TpuCDrw9",
-  proxy: { scheme: "socks5", host: "127.0.0.1", port: 1080 },
-}))
+console.log(
+  await instance.Solve({
+    type: "Turnstile",
+    url: "https://web.nebula-media.org",
+    sitekey: "0x4AAAAAAADGwT_-TpuCDrw9",
+    proxy: { scheme: "socks5", host: "127.0.0.1", port: 1080 },
+  }),
+);
 
-
-await instance.Close()
+await instance.Close();
